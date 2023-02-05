be7b4c8d6be1017610442a600ba8c51987f26b47
TamaraSokolovska <sokolovskatamara@yahoo.com>
2023-02-05 19:11:15 +0100

    Working solution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bson import json_util
 from flask import Flask
 import sqlite3
 import pandas as pd
@@ -6,7 +7,8 @@ app = Flask(__name__)
 import re
 import pymongo
 from pymongo import MongoClient
-
+import flask
+import json
 
 
 
@@ -17,18 +19,50 @@ def transformString(st):
     transform2 = re.sub(r',.*','',transform1)
     return transform2.replace(' - ','').replace('LIMITED', '').replace ('limited', '').replace('Limited', '').replace('LTD.', '').replace('LTD', '').lower().title()
 
-@app.route('/')
-def home():
+@app.route('/insert_to_mongo')
+def insert_into_mongo():
     connector = sqlite3.connect('semos_companies_data.db')
-    df = pd.read_sql("SELECT * FROM companies", con = connector)
-    df['company_name_cleaned']= df['name'].apply(transformString)
-    display(df[['company_name_cleaned','name']])
-    #df.to_sql(name = 'companies',if_exists = 'replace',con = connector)
+    df = pd.read_sql("SELECT * FROM companies", con=connector)
+    df = df.drop(['name'], axis=1)
     client = MongoClient('localhost', 27017)
     db = client.semos_database
     collection = db.companies
-    collection.insert_many(df.to_dict('records'))
-    return "stignav"
+    final_list = []
+    for item in df.to_dict('records'):
+        final_dct = {}
+        key = item.pop('company_name_cleaned')
+        final_dct[key] = item
+        final_list.append(final_dct)
+
+    collection.insert_many(final_list)
+    return "Finished inserting into MongoDB database"
+
+@app.route("/clear_data")
+def clear_data():
+    connector = sqlite3.connect('semos_companies_data.db')
+    df = pd.read_sql("SELECT * FROM companies", con = connector)
+    df['company_name_cleaned']= df['name'].apply(transformString)
+    display(df)
+    df.to_sql(name = 'companies',if_exists = 'replace',con = connector, index = False)
+    return "Finished transforming the data and writing to SQL database"
+
+
+@app.route('/show_data')
+def show_mongo_results():
+    client = MongoClient('localhost', 27017)
+    db = client.semos_database
+    collection = db.companies
+    res_cursor = collection.find({}).limit(20)
+    lst = []
+    for item in res_cursor:
+        lst.append(json.loads(json_util.dumps(item)))
+
+    return lst
+
+
+@app.route('/')
+def home():
+    return flask.render_template('main.html')
 
 
 
